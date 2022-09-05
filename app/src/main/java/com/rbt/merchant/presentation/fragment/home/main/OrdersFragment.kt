dea722495bb5a4d0bab8e442a614afeb95312af2
Heba Elsaid <72816466+hebaelsaid912@users.noreply.github.com>
2022-09-05 11:39:23 +0200

    handle backing from orders fragment

--- app/src/main/java/com/rbt/merchant/presentation/fragment/home/main/OrdersFragment.kt
+++ app/src/main/java/com/rbt/merchant/presentation/fragment/home/main/OrdersFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 import com.rbt.merchant.R
 import com.rbt.merchant.databinding.FragmentOrdersBinding
+import com.rbt.merchant.presentation.fragment.home.main.chat.all_chats.ChatFragmentDirections
 import com.rbt.merchant.presentation.ui.MainActivity
 
 
@@ -22,6 +25,11 @@ class OrdersFragment : Fragment() {
         (activity as MainActivity?)!!.showNavDrawer(false)
         (activity as MainActivity?)!!.showFragmentTitle(true, R.string.current_orders)
         (activity as MainActivity?)!!.showProfileImage(true)
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigate(OrdersFragmentDirections.actionOrdersFragmentToHomeFragment2())
+            }
+        })
         return binding.root
     }
 
